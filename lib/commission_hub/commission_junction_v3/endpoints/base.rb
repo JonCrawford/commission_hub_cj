526b530377cc7a7a3dbf64e87eab8b6d9bb71927
Felipe JAPM <felipe.japm@gmail.com>
2017-05-03 16:53:35 -0300

    wip

--- lib/commission_hub/commission_junction_v3/endpoints/base.rb
+++ lib/commission_hub/commission_junction_v3/endpoints/base.rb
@@ -5,7 +5,7 @@ module CommissionHub
         
         def call(request_params={})
           request_params = { headers: { "Authorization" => authorization_token }}.merge(request_params)
-          connection.class.get(uri, request_params)
+          connection.class.get(full_uri, request_params)
         end
 
         def authorization_token
